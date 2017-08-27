02748d1d0baa1785b98eb8391caeb19b09f3f9c7
ZHIHAO93 <zhzheng@ucm.es>
2017-08-27 16:29:15 +0200

    Update MergeTwoSortedList.java

--- src/leetCode/MergeTwoSortedList.java
+++ src/leetCode/MergeTwoSortedList.java
@@ -5,26 +5,26 @@ public class MergeTwoSortedList {
 	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 		ListNode tmp = new ListNode(0);
 		ListNode result = tmp;
-        while(l1 != null || l2 != null) {
-            if(l1 != null && l2 != null) {
-            	if(l1.val > l2.val) {
-            		tmp.next = new ListNode(l2.val);
-            		l2 = l2.next;
-            	}
-            	else {
-            		tmp.next = new ListNode(l1.val);
-            		l1 = l1.next;
-            	}
-            } else if (l1 != null) {
-                tmp.next = new ListNode(l1.val);
-                l1 = l1.next;
-            } else {
-                tmp.next = new ListNode(l2.val);
-                l2 = l2.next;
-            }
-            tmp = tmp.next;
-        }
-        return result.next;
+		while(l1 != null || l2 != null) {
+		    if(l1 != null && l2 != null) {
+			if(l1.val > l2.val) {
+				tmp.next = new ListNode(l2.val);
+				l2 = l2.next;
+			}
+			else {
+				tmp.next = new ListNode(l1.val);
+				l1 = l1.next;
+			}
+		    } else if (l1 != null) {
+			tmp.next = new ListNode(l1.val);
+			l1 = l1.next;
+		    } else {
+			tmp.next = new ListNode(l2.val);
+			l2 = l2.next;
+		    }
+		    tmp = tmp.next;
+		}
+		return result.next;
 	}
 	
 	class ListNode {
